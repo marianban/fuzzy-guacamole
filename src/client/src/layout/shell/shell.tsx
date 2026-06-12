1a16328080b39ba93b7f6cfd5a8117f79d0b5984
Marian Ban <marian.ban@gmail.com>
2026-06-12 19:57:00 +0200

    basic layout

--- src/client/src/layout/shell/shell.tsx
+++ src/client/src/layout/shell/shell.tsx
@@ -1,11 +1,22 @@
+import { useTranslation } from 'react-i18next';
+import { SidePanel } from '../../components/side-panel/side-panel';
 import { Header } from '../header/header';
 import styles from './shell.module.css';
 
 export const Shell = ({ children }: { children: React.ReactNode }) => {
+  const { t } = useTranslation();
   return (
     <div className={styles.shell}>
       <Header />
-      {children}
+      <main className={styles.canvas}>{children}</main>
+      <div className={styles.sidePanel}>
+        <SidePanel
+          title={t('ControlPanel.Title', 'Control Panel')}
+          content={<div>controls</div>}
+          footer={<div>footer</div>}
+        />
+      </div>
+      <footer className={styles.footer}></footer>
     </div>
   );
 };
