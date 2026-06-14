961b205767037cae99373dd7878516d3f1e3bc17
Marian Ban <marian.ban@gmail.com>
2026-06-14 19:46:25 +0200

    fixes i18next config on windows

--- src/client/i18next.config.ts
+++ src/client/i18next.config.ts
@@ -3,5 +3,11 @@ export default {
   extract: {
     input: 'src/**/*.{js,jsx,ts,tsx}',
     output: 'public/locales/{{language}}/{{namespace}}.json'
+  },
+  types: {
+    input: 'public/locales/en/*.json',
+    output: 'src/@types/i18next.d.ts',
+    resourcesFile: 'src/@types/resources.d.ts',
+    enableSelector: true
   }
 };
